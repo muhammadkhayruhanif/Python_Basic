f6d505fd839754cd2b89b3d2c17c1ed11f9a4bed
KHanif <m.khayruhanif28@gmail.com>
2025-05-01 20:52:28 +0700

    memperbarui __init__

--- sains/__init__.py
+++ sains/__init__.py
@@ -1,3 +1,5 @@
 __all__ = [
     "scientific"
 ]
+
+from sains.belajar_init.scientific import sinus,cosinus
--- sains/belajar_init/__init__.py
+++ sains/belajar_init/__init__.py
@@ -1,6 +1,6 @@
 # fungsi __init__ bisa untuk mengeluarkan hasil ketika mengimport tanpa memasukkan perintah
 # bisa untuk memasukkan import agar dapat lebih mudah di-run
-from . import belajar_init
+from sains.belajar_init.scientific import sinus,cosinus
 
 __all__ = [
     "scientific"
